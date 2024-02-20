47ac326270a2aeb901ef5cc67c7ab4cc20dbf511
Gustavo Barcelos <gustavo.qmb@gmail.com>
2024-02-20 08:10:40 -0300

    Commit inicial

---
+++ src/test/java/br/com/oak/casadocodigoapi/CasadocodigoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oak.casadocodigoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
